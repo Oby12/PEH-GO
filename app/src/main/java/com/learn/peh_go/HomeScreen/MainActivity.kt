5a2b1b94f710608c4765edaa9bdbadc19a024e47
Bobby  Saputra <bobbysaputra514@gmail.com>
2024-04-18 02:25:05 +0700

    First Commit create shape and background

---
+++ app/src/main/java/com/learn/peh_go/HomeScreen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.learn.peh_go.HomeScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.learn.peh_go.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
